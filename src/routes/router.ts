4496065365b6760b77b7acc591257dfd1402d2d8
Leonardo Oliveira <leozartino@gmail.com>
2022-02-19 14:47:14 -0300

    feat: add routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,14 +1,27 @@
 import express from "express";
-import { getActivesClass, postClass } from "../controllers/class.controller";
-import { postProfessor } from "../controllers/professor.controller";
+import {
+    getActivesClass,
+    postClass,
+    putClassModule,
+} from "../controllers/class.controller";
+import {
+    getStudentsByName,
+    postStudent,
+} from "../controllers/student.controller";
+// import { postProfessor } from "../controllers/professor.controller";
 
 const router = express.Router();
 
-//Turma
-router.post("/class", postClass);
-router.get("/class/active", getActivesClass);
+//Class
+router.post("/classes", postClass);
+router.get("/classes/active", getActivesClass);
+router.put("/classes/module", putClassModule);
+
+//Student
+router.post("/students", postStudent);
+router.get("/students/:name", getStudentsByName);
 
 //Professor
-router.post("/professor", postProfessor);
+// router.post("/professor", postProfessor);
 
 export default router;
