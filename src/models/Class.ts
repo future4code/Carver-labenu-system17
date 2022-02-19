6a5f79beaf03623d6cccfc40d3f8ff5941522baa
Leonardo Oliveira <leozartino@gmail.com>
2022-02-19 14:44:08 -0300

    feat: add typeing for module

--- src/models/Class.ts
+++ src/models/Class.ts
@@ -1,15 +1,14 @@
-// Fazer enum do modulo
+// import Professor from "./Professor";
+// import Student from "./Student";
 
-import { TableName } from "../constants/tables";
-import Professor from "./Professor";
-import Student from "./Student";
+import { ModuleValues } from "../constants/module";
 
 export default class Class {
     private id: string;
     private name: string;
     //private professors: Professor[];
     // private students: Student[];
-    private module: number;
+    private module: ModuleValues;
 
     constructor(id: string, name: string, module: number) {
         this.id = id;
@@ -23,7 +22,7 @@ export default class Class {
     public getId(): string {
         return this.id;
     }
-    public getModule(): number {
+    public getModule(): ModuleValues {
         return this.module;
     }
 }
