1c97bcc2f3cc31c86381d7bce5d64ce7deba1307
Leonardo Oliveira <leozartino@gmail.com>
2022-02-19 14:46:42 -0300

    feat: add person gets and date validation

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -2,13 +2,13 @@ export default abstract class Person {
     private id: string;
     private name: string;
     private email: string;
-    private birth_date: Date;
+    private birth_date: string;
     private class_id: string;
     constructor(
         id: string,
         name: string,
         email: string,
-        birth_date: Date,
+        birth_date: string,
         class_id: string
     ) {
         this.id = id;
@@ -16,5 +16,40 @@ export default abstract class Person {
         this.email = email;
         this.birth_date = birth_date;
         this.class_id = class_id;
+    }
+
+    public getId(): string {
+        return this.id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+    public getBirthDate(): string {
+        return this.birth_date;
+    }
+
+    public getClassId(): string {
+        return this.class_id;
+    }
+
+    public static isValidDate(date: any): boolean {
+        const regEx = /^\d{4}-\d{2}-\d{2}$/;
+        if (!date.match(regEx)) {
+            return false;
+        }
+
+        const test_date = new Date(date);
+        const dNum = test_date.getTime();
+
+        if (!dNum && dNum !== 0) {
+            return false; // NaN value, Invalid date
+        }
+        return test_date.toISOString().slice(0, 10) === date;
     }
 }
